6329d9c65abb99ee164e0dc1745515a68469a8ca
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2023-05-06 00:20:52 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -23,7 +23,7 @@ with open("output.html", "w") as file:
          if catches.group(1) in tags:
            file.write('<' + tags[catches.group(1)] + '>\n')
          elif catches.group() in tags and catches.group(3) in attr:
-            file write('<' + tags[catches.group(1)] + ' ' + attr[catches.group(3)] + catches.group(2).replace(catches.group(3)) + '>\n')
+            file write('<' + tags[catches.group(1)] + ' ' + attr[catches.group(3)] + catches.group(2).replace(catches.group(3),'') + '>\n')
          else:
            file.write('</' + tags[catches.group(1)[1:]] + '>/n')
         else:
