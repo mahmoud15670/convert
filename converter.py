89a924b2e3f71f9510d55acf65894f71feddbbb6
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2023-05-05 19:42:57 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,11 +1,20 @@
 from re import search
 
 doc=[]
+tags={
+   'تعريف الملف':"!DOCTYPE html"
+}
 with open("input.txt") as file:
     for row in file:
         doc.append(row)
 
+print(doc)
 with open("output.html", "w") as file:
     for row in doc:
-        if catches := search(r'<(/?[ابجدهوزكلمنحطىسعفصقرشتثخذضظغية ]+)>', row):
-            print(catches.group(1))
+        if catches := search(r'^<(/?[ابجدهوزكلمنحطى سعفصقرشتثخذضظغية]+)>$', row):
+         if catches.group(1) in tags:
+           file.write('<' + tags[catches.group(1)] + '>\n')
+         else:
+           file.write('</' + tags[catches.group(1)[1:]] + '>/n')
+        else:
+          file.write(row)
