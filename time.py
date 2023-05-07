993f38e7fdfe59f2a4ba20363e2647a87d057f69
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2023-05-08 01:33:03 +0300

    Update time.py

--- time.py
+++ time.py
@@ -2,6 +2,8 @@ class User:
     def __init__(self, name, password):
         if not name:
             raise ValueError('missing name')
+        if not password:
+            raise ValueError('missing password)
         self.name = name
         self.password = password
 
@@ -12,4 +14,7 @@ class User:
 
 name = input('the name ')
 password = input('password ')
-user = User(name, password)
+try:
+    user = User(name, password)
+except ValueError:
+    print('you missed an input')
