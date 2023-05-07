4e91536c199694adf2c238240237ad05c80ae22c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2023-05-07 12:03:35 +0200

    style

--- converter.py
+++ converter.py
@@ -2,29 +2,29 @@ from re import search
 
 doc=[]
 tags={
-   'تعريف الملف':"!DOCTYPE html",
-   'البداية':"html",
-   'الراس':"head",
-   'الجسم':"body",
-   'فقرة':"p"
+  'تعريف الملف':"!DOCTYPE html",
+  'البداية':"html",
+  'الراس':"head",
+  'الجسم':"body",
+  'فقرة':"p"
 }
 attr={
   'تصميم':"style",
   'لينك':"herf"
 }
 with open("input.txt") as file:
-    for row in file:
-        doc.append(row)
+  for row in file:
+    doc.append(row)
 
 print(doc)
 with open("output.html", "w") as file:
-    for row in doc:
-        if catches := search(r'^<(/?[ابجدهوزكلمنحطى سعفصقرشتثخذضظغية]+)( ([ابجدهوزكلمنحطى سعفصقرشتثخذضظغي]+)=".+")?>$', row):
-         if catches.group(1) in tags:
-           file.write('<' + tags[catches.group(1)] + '>\n')
-         elif catches.group() in tags and catches.group(3) in attr:
-            file write('<' + tags[catches.group(1)] + ' ' + attr[catches.group(3)] + catches.group(2).replace(catches.group(3),'') + '>\n')
-         else:
-           file.write('</' + tags[catches.group(1)[1:]] + '>/n')
-        else:
-          file.write(row)
+  for row in doc:
+    if catches := search(r'^<(/?[ابجدهوزكلمنحطى سعفصقرشتثخذضظغية]+)( ([ابجدهوزكلمنحطى سعفصقرشتثخذضظغي]+)=".+")?>$', row):
+      if catches.group(1) in tags:
+        file.write('<' + tags[catches.group(1)] + '>\n')
+      elif catches.group() in tags and catches.group(3) in attr:
+        file.write('<' + tags[catches.group(1)] + ' ' + attr[catches.group(3)] + catches.group(2).replace(catches.group(3),'') + '>\n')
+      else:
+        file.write('</' + tags[catches.group(1)[1:]] + '>/n')
+    else:
+      file.write(row)
